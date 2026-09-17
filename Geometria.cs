708b3e007160e66e144bd34480dbc5bd707be93c
43947679890 <43947679890@ECE337N25SN790>
2026-09-17 07:30:57 -0300

    Adicionar arquivos de projeto.

---
+++ Geometria.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Geometria
+    {
+        public double CalcularArea(double lado)
+        {
+            return lado * lado;
+        }
+        public double CalcularArea(double @base, double altura)
+        {
+            return @base * altura;
+        }
+        public double CalcularArea(double raio, bool isCirculo)
+        {
+            return Math.PI * raio * raio;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Program
+    {
+        static void Main()
+        {
+            Geometria geometria = new Geometria();
+            Console.WriteLine(
+                $"Área do quadrado: {geometria.CalcularArea(4):F2}");
+
+            Console.WriteLine(
+                $"Área do retângulo: {geometria.CalcularArea(5, 3):F2}");
+
+            Console.WriteLine(
+                $"Área do círculo: {geometria.CalcularArea(2, true):F2}");
+        }
+    }
+}
