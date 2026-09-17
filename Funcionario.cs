5436564415bbb21bc7855adcf81d414029a57362
43947679890 <43947679890@ECE337N25SN790>
2026-09-17 07:57:23 -0300

    Polimorfismo

---
+++ Funcionario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Funcionario
+    {
+        public string Nome { get; set; } = "";
+        public double Salario { get; set; }
+        public virtual double CalcularBonusAnual()
+        {
+            return Salario * 0.10;
+        }
+    }
+}
---
+++ Gerente.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Gerente : Funcionario
+    {
+        public override double CalcularBonusAnual()
+        {
+            return Salario * 0.15 + 1000;
+        }
+    }
+}
