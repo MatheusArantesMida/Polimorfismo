b1108d71525d3231dac2117fd44b3dc48f6d26ad
43947679890 <43947679890@ECE337N25SN790>
2026-09-17 08:12:22 -0300

    Polimorfismo

---
+++ Animal.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Animal
+    {
+        public virtual void EmitirSom()
+        {
+            Console.WriteLine("Som de animal.");
+        }
+    }
+}
---
+++ Cobra.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polimorfismo
+{
+    internal class Cobra : Animal
+    {
+        public override void EmitirSom()
+        {
+            Console.WriteLine("Sssssss...");
+        }
+    }
+}
